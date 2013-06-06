4cbf5d2e5f6ab50d7484759e8d19fe399d081ac3
Achim Zeileis <Achim.Zeileis@R-project.org>
2013-06-06 00:00:00 +0000

    version 1.9-6

--- tests/encodings.R
+++ tests/encodings.R
@@ -1,5 +1,5 @@
 if(!identical(Sys.getlocale(), "C")) {
 library("exams")
-exams2pdf("currency8", encoding = "utf8",   template = "plain8", dir = ".")
-exams2pdf("currency9", encoding = "latin9", template = "plain9", dir = ".")
+exams2pdf("currency8", encoding = "UTF-8",   template = "plain8", dir = ".")
+exams2pdf("currency9", encoding = "Latin-9", template = "plain9", dir = ".")
 }
